634adc5f28692ea52183d0952bad414f27e5dbf8
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-15 17:35:08 -0400

    update index page to only show most recent 6 photos

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,13 @@
 class Picture < ApplicationRecord
+
+  def self.newest_first
+    Picture.order("created_at DESC")
+  end
+
+  def self.most_recent_six
+    Picture.newest_first.limit(6)
+  end
+
+
+
 end
